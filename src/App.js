592e4c89dec17fca6bae5ae8b551bbf8e2354fd7
Jehoon <limjhoon8@gmail.com>
2024-06-13 09:56:01 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React from "react";
 import ExpenseList from "./components/expenses/ExpenseList";
 import Greet from "./components/Greet";
+import Counter from "./components/practice/Counter";
 
 const App = () => {
   // jsx 규칙
@@ -33,6 +34,7 @@ const App = () => {
 
   return (
     <>
+    <Counter />
       <ExpenseList expenses={expenses}/>
       <Greet>
         <ul>
--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -27,17 +27,28 @@ const ExpenseItem = ({ date, title, price: exPrice }) => {
 
     return `${year}년 ${make2digit(month)}월 ${make2digit(day)}일`;
   };
+  
+
+  // useState는 컴포넌트의 상태값을 관리하며 이 상태값은 렌더링에 영향을 미침
+  /*
+    - useState훅의 리턴값은 배열이며
+    - 첫번째 요소는 관리할 상태값의 초기값
+    - 두번째 요소는 해당 상태값을 변경할 때 사용하는 setter함수
+  */
+  const [itemTitle, setItemTitle] = useState(title);
+  // console.log(abc);
+
+
 
   // 원화 표기법으로 변환
   const formattedPrice = new Intl.NumberFormat('ko-KR').format(exPrice);
 
   // 이벤트 핸들러 선언
   const clickHandler = e => {
-    console.log('클릭함!');
-    console.log(e.target.previousElementSibling.firstElementChild.textContent);
-    
-    // const $price = document.querySelectorAll('.expense-item__price');
-    // console.log($price);
+    /*
+      useState가 관리하는 상태값은 반드시 setter로만 변경해야 합니다.
+    */
+    setItemTitle('짜자면');
   };
 
 
@@ -45,7 +56,7 @@ const ExpenseItem = ({ date, title, price: exPrice }) => {
       <Card className='expense-item'>
         <ExpenseDate exDate={date} />
         <div className='expense-item__description'>
-          <h2>{title}</h2>
+          <h2>{itemTitle}</h2>
           <div className='expense-item__price'>{formattedPrice}원</div>
         </div>
         <button id='btn' onClick={clickHandler}>버튼1</button>
