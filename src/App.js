619ddfcd2efe6a915f8b9514b748d919b1928ed4
Jehoon <limjhoon8@gmail.com>
2024-06-20 10:55:08 +0900

    # 타이머 시작이벤트 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Player from './components/Ref/Player';
+import TimerChallenge from './components/Ref/TimeChallenge';
 
 
 const App = () => {
@@ -7,7 +8,12 @@ const App = () => {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title='Easy' targetTime={1} />
+        <TimerChallenge title='Not Easy' targetTime={5} />
+        <TimerChallenge title='Getting tough' targetTime={10} />
+        <TimerChallenge title='Pros only' targetTime={15} />
+      </div>
     </>
   );
 };
