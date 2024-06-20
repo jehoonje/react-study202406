c710c1d83d98a9f5b41525b4648248613ba5b1f6
Jehoon <limjhoon8@gmail.com>
2024-06-20 11:15:56 +0900

    # stop이 안되는 원인2

--- src/components/Ref/TimeChallenge.js
+++ src/components/Ref/TimeChallenge.js
@@ -1,34 +1,32 @@
-import React , { useState }from 'react';
+import React, { useState } from 'react';
+
+let timer;
 
 const TimerChallenge = ({ title, targetTime }) => {
 
   // 타이머가 시작되었는지를 확인하는 상태값
   const [timerStarted, setTimerStarted] = useState(false);
 
-
   // 타겟시간이 종료되었는지 여부
   const [timerExpired, setTimerExpired] = useState(false);
 
-  let timer;
+  
   const startHandler = e => {
 
     timer = setTimeout(() => {
-      console.log('시간 만료!');
       setTimerExpired(true);
     }, targetTime * 1000);
 
     setTimerStarted(true);
   };
 
-  // 이 상황에 stop이 작동하지 않는 이유는
-  // start시의 timer변수가 지역변수이기 때문에
-  // 상태변수의 setter호출시 리렌더링이 되면서 새로운 지여견수로 바뀜
-  // stop시의 timer와 start시의 timer는 다른 변수다.
-
+  // 전역변수로 timer 설정시 5초 -> 1초 -. 1초 -> 5초를 연속 클릭해보면
+  // 5초짜리 timer가 정상작동하지 않는 이유는
+  // 4개의 TimeChallenge 컴포넌트가 1개의 timer를 공유하여
+  // 처음 5초짜리 timer가 1초 짜리에 의해 덮어씌워지기 때문
   const stopHandler = e => {
-    console.log("STOP!");
     clearTimeout(timer);
-  }
+  };
 
   return (
     <section className="challenge">
@@ -38,7 +36,7 @@ const TimerChallenge = ({ title, targetTime }) => {
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button onClick={timerStarted ? stopHandler : startHandler}> 
+        <button onClick={timerStarted ? stopHandler : startHandler}>
           {timerStarted ? 'Stop' : 'Start'} Challenge
         </button>
       </p>
