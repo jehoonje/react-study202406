ab76a903f7da587321b7378e08a8154fd7a9ebb1
Jehoon <limjhoon8@gmail.com>
2024-06-24 10:42:24 +0900

    # 장바구니 + 버튼 처리, 장바구니 중복상품 수량 합계처리

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,5 +1,6 @@
 import React, { useReducer, useState } from 'react';
 import CartContext from './cart-context';
+import { upload } from '@testing-library/user-event/dist/upload';
 
 // 중앙관리 상태값 (state)
 const defaultState = {
@@ -20,13 +21,30 @@ const cartReducer = (state, action) => {
 
     // 상태 업데이트 코드
     // 장바구니 배열 상태 업데이트
-    const updateCartItems = [...state.items, action.value];
+
+    // 장바구니에 추가될 신규 아이템
+    const newCartItem = action.value;
+
+    // 기존에 등록된 메뉴인지 확인해보기 위해 해당 아이템의 인덱스를 탐색
+    const index = state.items.findIndex(item => item.id === newCartItem.id);
+
+    // 기존에 존재하는 아이템배열 사본
+    const existingItems = [...state.items];
+
+    // 신규 아이템인 경우
+    let updatedItems;
+    if (index === -1) {
+      updatedItems = [...existingItems, newCartItem];
+    } else { // 이미 장바구니에 있었던 상품의 추가 : 수량만 업데이트
+      existingItems[index].amount += newCartItem.amount;
+      updatedItems = [...existingItems];
+    }
 
     // 총액 상태 업데이트
     const updatePrice = state.totalPrice + (action.value.price * action.value.amount);
 
     return {
-      items: updateCartItems,
+      items: updatedItems,
       totalPrice: updatePrice
     }; // 새로운 상태
   } else if (action.type === 'REMOVE') { // 장바구니 제거
